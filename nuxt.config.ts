7883d47e083a5632ae7e562c3ca643de22c92a17
LehuyH <44150458+LehuyH@users.noreply.github.com>
2024-07-23 03:51:45 -0700

    chore: prerender scenes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,11 @@
+const map = {
+  "ovid-pygmalion": ["intro","creating-galatea", "venus-prayer", "transformation"],
+  "shaw-pygmalion": ["act-1","act-2", "act-3", "act-4", "act-5"],
+  "lerner-my-fair-lady": ["act-1","act-2", "alfred", "training", "eliza-transforms", "act-3", 
+      "freddy","act-4", "act-5"],
+} as Record<string,string[]>
+
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
@@ -9,5 +17,11 @@ export default defineNuxtConfig({
     "@nuxt/content",
     "@nuxt/icon"
   ],
-  ssr: false
+  ssr: true,
+  nitro: {
+    preset: "node-server",
+    prerender:{
+      routes: Object.keys(map).flatMap(story=>map[story].map(scene=>`/scene/${story}/${scene}`))
+    }
+  },
 })
